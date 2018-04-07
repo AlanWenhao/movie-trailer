b733e1d5dc2c6f83b4b5c463aca9eeec7b447ddd
AlanWenhao <13454782783@163.com>
2018-04-07 14:58:12 +0800

    init project

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const Koa = require('koa');
+const { normal } = require('./tpl');
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+    ctx.type = 'text/html; charset=utf-8'
+    ctx.body = normal;
+});
+
+app.listen(8080);
---
+++ server/tpl/normal.js
@@ -0,0 +1,25 @@
+module.exports = `
+    <!DOCTYPE html>
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+            <script src="https://cdn.bootcss.com/jquery/3.2.0/jquery.min.js"></script>
+            <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8">
+                        <h1>Hi Alan</h1>
+                        <p>This is Alan</p>
+                    </div>
+                    <div class="col-md-4">
+                        <p>This is a static page</p>
+                    </div>
+                </div>
+            </div>
+            
+        </body>
+    </html>
+`
